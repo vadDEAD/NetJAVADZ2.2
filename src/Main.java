026bedeed4b5625f2922163ef892cb8dc89fe4da
vaDEAD <t0rm0z.vad.tru@gmail.com>
2022-12-15 02:26:32 +0300

    DZ2.2new

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
 public class Main {
     public static void main(String[] args) {
 
-        int oldBalance = 10; // баланс до пополнения
-        int Payment = 2000; // сумма пополнения
+        int oldBalance = 1000; // баланс до пополнения
+        int Payment = 1000; // сумма пополнения
         int newBalance; // баланс после пополнения
         int Bonus_rub; // бонусные рубли
         Bonus_rub = Payment / 100; // Расчет бонуса к зачислению
+        if (Payment == 0) {
+            System.out.println("Пополнение на ноль невозможно");
+        }
         if (Payment < 1000)  // условие начисления бонуса
         {
             newBalance = oldBalance + Payment;
-            System.out.println("Пополнение счета на: " + Payment + "р. Сумма вашего счета составляет: " + newBalance);
+            System.out.println("Пополнение счета на: " + Payment + "р. Сумма вашего счета составляет: " + newBalance + "р.");
         } else {
             newBalance = oldBalance + Payment + Bonus_rub;
             System.out.println("Пополнение счета на: " + Payment + "р. Сумма вашего счета составляет: " + newBalance + "р. Вам начислено: " + Bonus_rub + "р. бонусом.");
